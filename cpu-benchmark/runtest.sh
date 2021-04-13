b9c28902f18f27256b7d10c6f288216c4db59aaf
Arun Kumar <arun6582@gmail.com>
2021-04-14 05:22:59 +0530

    configurable cpu test

--- cpu-benchmark/runtest.sh
+++ cpu-benchmark/runtest.sh
@@ -1,7 +1,7 @@
 curl https://raw.githubusercontent.com/arun6582/public/master/cpu-benchmark/cpu-test --output cpu-test
 chmod +x cpu-test
-echo 'Running test'
-time ./cpu-test 100 1000000
-echo 'Running 10 * test'
-time ./cpu-test 100 10000000
+echo "Running test $2 times"
+time ./cpu-test $1 $2
+echo "Running test $(($2 * 10)) times"
+time ./cpu-test $1 $(($2 * 10))
 rm -rf ./cpu-test
--- readme.md
+++ readme.md
@@ -1,7 +1,7 @@
 # benchmark
 
 ### cpu test
-`curl -sL https://raw.githubusercontent.com/arun6582/public/master/cpu-benchmark/runtest.sh | bash -`
+`curl -sL https://raw.githubusercontent.com/arun6582/public/master/cpu-benchmark/runtest.sh | bash -s -- 100 1000000`
 
 ### disk test
 
