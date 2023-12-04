7fb248ab855dfe57df43d7459e2707ab542ed06a
Arun Kumar <arun6582@gmail.com>
2023-12-04 19:40:47 +0530

    terminal helpers: Mon Dec  4 19:40:47 IST 2023

---
+++ bin/free-local-space.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -x
+
+SNAP_FOLDER=$1
+
+logmsg "*********free local space start"
+
+if [[ ! -z $SNAP_FOLDER ]]; then
+  logmsg 'cleaning snaps folder'
+  find $SNAP_FOLDER -maxdepth 1 -mtime +60 | xargs -I {} rm -rf "{}"
+fi
+
+# free docker files
+/usr/local/bin/docker system prune -af --volumes
+
+clean_service_worker() {
+  cd "$1"
+  find . -type d -name "Service Worker" | xargs -I {} rm -rf {}
+}
+clean_service_worker '/Users/arun/Library/Application Support/Thorium/'
+clean_service_worker '/Users/arun/Library/Application Support/Google/Chrome'
+
+# free browser cache
+rm -rf $HOME/Library/Caches/Google
+rm -rf $HOME/Library/Caches/Thorium
+rm -rf $HOME/Library/Caches/Firefox
+
+# free trash
+ls ~/.Trash/ | xargs -I {} rm -rf "$HOME/.Trash/{}"
+
+logmsg "*********free local space finished"
