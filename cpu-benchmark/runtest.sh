594d8a5819d1b0e2829f48e2684747b5d5337465
Arun Kumar <arun6582@gmail.com>
2026-07-20 15:28:46 +0530

    [master] run test multiple times

--- cpu-benchmark/runtest.sh
+++ cpu-benchmark/runtest.sh
@@ -1,8 +1,16 @@
 file=/tmp/cpu-test
 curl https://raw.githubusercontent.com/arun6582/public/master/cpu-benchmark/cpu-test --output $file
 chmod +x $file
-echo "Running test $2 times"
-time $file $1 $2
-echo "Running test $(($2 * 10)) times"
-time $file $1 $(($2 * 10))
-rm -rf $file
+
+job() {
+    echo "TEST - #$1"
+    time $file $2 $3
+}
+
+
+set -eu
+
+for i in {1..3}
+do
+  job $i $1 $2
+done
