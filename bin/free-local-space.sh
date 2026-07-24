a44cb96495c592b560b5f0bafffd7077693fa610
Arun Kumar <arun6582@gmail.com>
2026-07-25 01:04:47 +0530

    terminal helpers: Sat Jul 25 01:04:47 IST 2026

--- bin/free-local-space.sh
+++ bin/free-local-space.sh
@@ -14,23 +14,14 @@ fi
 # free docker files
 /usr/local/bin/docker system prune -af --volumes
 
-clean_service_worker() {
-  cd "$1"
-  find . -type d -name "Service Worker" | xargs -I {} rm -rf {}
-}
-clean_service_worker '/Users/arun/Library/Application Support/Thorium/'
-clean_service_worker '/Users/arun/Library/Application Support/Google/Chrome'
-
-# free browser cache
-rm -rf $HOME/Library/Caches/Google
-rm -rf $HOME/Library/Caches/Thorium
-rm -rf $HOME/Library/Caches/Firefox
-
 # free trash
 ls ~/.Trash/ | xargs -I {} rm -rf "$HOME/.Trash/{}"
 
 # free npm cache
 rm -rf ~/.npm/_cacache/tmp/*
+
+# clear nix temps
+nix-collect-garbage
 
 logmsg "*********free local space finished"
 # notify 'Local space freed'
