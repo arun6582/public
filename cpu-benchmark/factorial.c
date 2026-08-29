6b0efa31a4832e05472ede60bca5bcfbad895be3
Arun Kumar <arun6582@gmail.com>
2026-08-29 22:24:59 +0530

    [master] multi core score

--- cpu-benchmark/factorial.c
+++ cpu-benchmark/factorial.c
@@ -1,7 +1,109 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 #include <string.h>
+
+#include <pthread.h>
+#include <stdint.h>
+#include <time.h>
+
+#ifdef __APPLE__
+#include <sys/types.h>
+#include <sys/sysctl.h>
+#else
+#include <unistd.h>
+#endif
+
+#define ITERATIONS 500000000ULL
+
+typedef struct
+{
+    uint64_t iterations;
+    double result;
+} worker_args;
+
+static int cpu_count(void)
+{
+#ifdef __APPLE__
+    int count;
+    size_t size = sizeof(count);
+
+    sysctlbyname("hw.logicalcpu", &count, &size, NULL, 0);
+
+    return count;
+#else
+    return (int)sysconf(_SC_NPROCESSORS_ONLN);
+#endif
+}
+
+static double now_seconds(void)
+{
+    struct timespec ts;
+
+    clock_gettime(CLOCK_MONOTONIC, &ts);
+
+    return (double)ts.tv_sec +
+           (double)ts.tv_nsec / 1e9;
+}
+
+static void *worker(void *arg)
+{
+    worker_args *a = arg;
+
+    double x = 1.0;
+
+    for (uint64_t i = 0; i < a->iterations; i++)
+        x = x * 1.0000001 + 0.0000001;
+
+    a->result = x;
+
+    return NULL;
+}
+
+double multicore_benchmark(void)
+{
+    int cores = cpu_count();
+    printf("Cores: %d\n", cores);
+
+    pthread_t *threads =
+        malloc(sizeof(*threads) * cores);
+
+    worker_args *args =
+        malloc(sizeof(*args) * cores);
+
+    double start = now_seconds();
+
+    for (int i = 0; i < cores; i++)
+    {
+        args[i].iterations = ITERATIONS;
+        args[i].result = 0.0;
+
+        pthread_create(
+            &threads[i],
+            NULL,
+            worker,
+            &args[i]);
+    }
+
+    double result = 0.0;
+
+    for (int i = 0; i < cores; i++)
+    {
+        pthread_join(threads[i], NULL);
+        result += args[i].result;
+    }
+
+    free(threads);
+    free(args);
+
+    /* Prevent compiler from eliminating the calculation. */
+    volatile double sink = result;
+    (void)sink;
+
+    double time = now_seconds() - start;
+    double score = 100.0 / time;
+    printf("Multi score Score:   %.2f\n", score);
+    return score;
+}
 
 unsigned long long factorial(int n)
 {
@@ -15,7 +117,7 @@ unsigned long long factorial(int n)
     return fact;
 }
 
-void f(int a, int reps)
+void factorials(int a, int reps)
 {
     for (int r = 0; r < reps; r++)
     {
@@ -28,14 +130,19 @@ void f(int a, int reps)
 
 double memory_bandwidth_gbps(void)
 {
-    const size_t size = 1024ULL * 1024 * 1024; // 1 GiB
+    const size_t size = 256ULL * 1024 * 1024; // 1 GiB
     const int reps = 10;
 
-    char *src = aligned_alloc(64, size);
-    char *dst = aligned_alloc(64, size);
+    char *src = malloc(size);
+    char *dst = malloc(size);
 
     if (!src || !dst)
+    {
+        free(src);
+        free(dst);
+        printf("Memory test failed\n");
         return 0.0;
+    }
 
     memset(src, 1, size);
     memset(dst, 0, size);
@@ -55,7 +162,37 @@ double memory_bandwidth_gbps(void)
     free(src);
     free(dst);
 
-    return ((double)size * reps * 2.0) / elapsed / 1e9;
+    double gbps = ((double)size * reps * 2.0) / elapsed / 1e9;
+    printf("%.2f GB/s\n", gbps);
+    return gbps;
+}
+
+double singlecore_benchmark(void (*f)(int, int), int n, int reps)
+{
+    double total_time = 0.0;
+    int total_reps = reps;
+
+    do
+    {
+        clock_t start = clock();
+
+        f(n, reps);
+
+        clock_t end = clock();
+
+        double run_time =
+            (double)(end - start) / CLOCKS_PER_SEC;
+
+        total_time += run_time;
+        total_reps += reps;
+
+    } while (total_time < 20.0);
+
+    double average = total_time / total_reps;
+    double score = 100.0 / average;
+
+    printf("Single core score:   %.2f\n", score);
+    return score;
 }
 
 int main(int argc, char *argv[])
@@ -72,30 +209,10 @@ int main(int argc, char *argv[])
     for (int test = 1; test <= 10; test++)
     {
         printf("TEST - #%d\n", test);
-        printf("%.2f GB/s\n", memory_bandwidth_gbps());
 
-        double total_time = 0.0;
-        int total_reps = reps;
-
-        do
-        {
-            clock_t start = clock();
-
-            f(n, reps);
-
-            clock_t end = clock();
-
-            double run_time =
-                (double)(end - start) / CLOCKS_PER_SEC;
-
-            total_time += run_time;
-            total_reps += reps;
-
-        } while (total_time < 20.0);
-
-        double average = total_time / total_reps;
-        double score = 100.0 / average;
-        printf("Score:   %.2f\n", score);
+        memory_bandwidth_gbps();
+        singlecore_benchmark(factorials, n, reps);
+        multicore_benchmark();
     }
 
     return 0;
